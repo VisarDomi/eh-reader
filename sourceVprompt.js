33e10b893962d961a7af68fe5f9f9e7f508dd260
Visar Domi <visardomi4@gmail.com>
2020-07-04 15:57:20 +0200

    fixed ordering

--- sourceVprompt.js
+++ sourceVprompt.js
@@ -21,6 +21,9 @@ function main() {
         )[0]
     );
     const REQUESTED_PAGES = NUMBER_THUMBNAIL_PAGES - BEGINNING_PAGE_NUMBER + 1; // total should be 2 (22-21+1)
+    for(let i=0;i<REQUESTED_PAGES;i++){
+      imageSources.push({id:i,source:""}) //populate the image sources with ids
+    }
     if (NUMBER_THUMBNAIL_PAGES > 200 && NUMBER_THUMBNAIL_PAGES <= 400) {
       let xhr1 = new XMLHttpRequest();
       xhr1.open("GET", window.location.href + "?p=1");
@@ -43,57 +46,58 @@ function main() {
 }
 function engine(REQUESTED_PAGES, thumbnailPages, imageSources, loadedLinks) {
   for (let i = 0; i < REQUESTED_PAGES; i++) {
-    setTimeout(function () {
-      let xhr2 = new XMLHttpRequest();
-      xhr2.open("GET", thumbnailPages[i]);
-      xhr2.send();
-      xhr2.onreadystatechange = function () {
-        if (xhr2.readyState === 4) {
-          const RESPONSE_TEXT_2 = xhr2.responseText;
-          const NL = RESPONSE_TEXT_2.split("nl('")[1].split("')")[0];
-          const URL_FROM_NL = thumbnailPages[i] + "?nl=" + NL;
-          let xhr3 = new XMLHttpRequest();
-          xhr3.open("GET", URL_FROM_NL);
-          xhr3.send();
-          xhr3.onreadystatechange = function () {
-            if (xhr3.readyState === 4) {
-              const RESPONSE_TEXT_3 = xhr3.responseText;
-              const IMAGE_SOURCE = RESPONSE_TEXT_3.split('src="')[5].split(
-                '"'
-              )[0];
-              imageSources.push(IMAGE_SOURCE); // async here, problems ahead
-              loadedLinks++;
-              let body = document.body;
-              let div = document.createElement("div");
+    let xhr2 = new XMLHttpRequest();
+    xhr2.open("GET", thumbnailPages[i]);
+    xhr2.send();
+    xhr2.onreadystatechange = function () {
+      if (xhr2.readyState === 4) {
+        const RESPONSE_TEXT_2 = xhr2.responseText;
+        const NL = RESPONSE_TEXT_2.split("nl('")[1].split("')")[0];
+        const URL_FROM_NL = thumbnailPages[i] + "?nl=" + NL;
+        let xhr3 = new XMLHttpRequest();
+        xhr3.open("GET", URL_FROM_NL);
+        xhr3.send();
+        xhr3.onreadystatechange = function () {
+          if (xhr3.readyState === 4) {
+            const RESPONSE_TEXT_3 = xhr3.responseText;
+            const IMAGE_SOURCE = RESPONSE_TEXT_3.split('src="')[5].split(
+              '"'
+            )[0];
+            if(imageSources[i].id===i){
+              imageSources[i].source = IMAGE_SOURCE;
+              // console.log(IMAGE_SOURCE)
+            }
+            loadedLinks++;
+            let body = document.body;
+            let div = document.createElement("div");
+            body.innerHTML = "";
+            body.style.margin = "0px";
+            body.style.backgroundColor = "#000000";
+            let percentage = loadedLinks / REQUESTED_PAGES * 100;
+            div.setAttribute(
+              "style",
+              "background-color:#aaaaaa;height:100px;width:" +
+                percentage +
+                "%;"
+            );
+            body.appendChild(div);
+            if (loadedLinks === REQUESTED_PAGES) {
               body.innerHTML = "";
-              body.style.margin = "0px";
-              body.style.backgroundColor = "#000000";
-              let percentage = loadedLinks / REQUESTED_PAGES;
-              div.setAttribute(
-                "style",
-                "background-color:#aaaaaa;height:100px;width:" +
-                  percentage * 100 +
-                  "%;"
-              );
-              body.appendChild(div);
-              if (loadedLinks === REQUESTED_PAGES) {
-                body.innerHTML = "";
-                let image = null;
-                for (let j = 0; j < REQUESTED_PAGES; j++) {
-                  setTimeout(function () {
-                    let divContainer = document.createElement("div");
-                    image = new Image();
-                    image.src = imageSources[j];
-                    divContainer.append(image);
-                    body.appendChild(divContainer);
-                  }, j * 500);
-                }
+              let image = null;
+              for (let j = 0; j < REQUESTED_PAGES; j++) {
+                setTimeout(function () {
+                  let divContainer = document.createElement("div");
+                  image = new Image();
+                  image.src = imageSources[j].source;
+                  divContainer.append(image);
+                  body.appendChild(divContainer);
+                }, j * 500); // load the images every 0.5 seconds
               }
             }
-          };
-        }
-      };
-    }, i * 100);
+          }
+        };
+      }
+    };
   }
 }
 main()
